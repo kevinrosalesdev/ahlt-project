8c8ea55b02e97ad52ba0ca4f14f385157f3a592e
kevinrosalesdev <kevinrosalesdev@gmail.com>
2021-02-20 00:26:35 +0100

    Utilization of external resources to improve F1-Score (11.7%)

--- baseline-NER.py
+++ baseline-NER.py
@@ -6,6 +6,28 @@ from xml.dom.minidom import parse
 from nltk.tokenize import word_tokenize, TreebankWordTokenizer as twt
 
 
+def load_drug_bank():
+    print("Loading Drug Bank...")
+    f = open("resources/DrugBank.txt", 'r')
+    lines = f.readlines()  # array of file lines
+    drug_bank_names = []
+    drug_bank_types = []
+    for line in lines:
+        line = line.strip().split("|")
+        drug_bank_names.append(line[0])
+        drug_bank_types.append(line[1])
+    return drug_bank_names, drug_bank_types
+
+
+def load_hsdb():
+    print("Loading HSDB...")
+    f = open("resources/HSDB.txt", 'r')
+    lines = f.readlines()  # array of file lines
+    hsdb_names = [line.strip() for line in lines]
+    hsdb_types = ['drug']*len(lines)
+    return hsdb_names, hsdb_types
+
+
 def tokenize(s):
     """
     Task:
@@ -23,10 +45,17 @@ def tokenize(s):
          ("common",32,37), ("cold",39,42), (".",43,43)]
     """
     raw_list = list(zip(word_tokenize(s), list(twt().span_tokenize(s))))
-    return [(token, offset_1, offset_2-1) for token, (offset_1, offset_2) in raw_list]
+    return [(token, offset_1, offset_2 - 1) for token, (offset_1, offset_2) in raw_list]
 
 
-def extract_entities(s):
+def use_external_resources(token, drug_bank_names, drug_bank_types):
+    try:
+        return drug_bank_types[drug_bank_names.index(token)]
+    except:
+        return 'drug'
+
+
+def extract_entities(s, drug_bank_names, drug_bank_types):
     """
     Task:
         Given a tokenized sentence, identify which tokens (or groups of
@@ -45,9 +74,10 @@ def extract_entities(s):
     entities = []
     for token in s:
         # TODO: Chain of Rules filtering & classifying tokens should be here!
+        type = use_external_resources(token[0], drug_bank_names, drug_bank_types)
         entities.append({"name": token[0],
                          "offset": f"{token[1]}-{token[2]}",
-                         "type": "drug"})
+                         "type": type})
 
     return entities
 
@@ -56,8 +86,14 @@ if __name__ == '__main__':
     datadir = sys.argv[1]
     outfile = sys.argv[2]
     outf = open(outfile, "w")
+    drug_bank_names, drug_bank_types = load_drug_bank()
+    hsdb_names, hsdb_types = load_hsdb()
+    drug_bank_names.extend(hsdb_names)
+    drug_bank_types.extend(hsdb_types)
+
     # process each file in directory
-    for f in listdir(datadir):
+    for idx, f in enumerate(listdir(datadir), 1):
+        print(f"Processing file nº {idx}/{len(listdir(datadir))}")
         # parse XML file, obtaining a DOM tree
         tree = parse(datadir + "/" + f)
         # process each sentence in the file
@@ -68,7 +104,7 @@ if __name__ == '__main__':
             # tokenize text
             tokens = tokenize(stext)
             # extract entities from tokenized sentence text
-            entities = extract_entities(tokens)
+            entities = extract_entities(tokens, drug_bank_names, drug_bank_types)
             # print sentence entities in format requested for evaluation
             for e in entities:
                 print(sid + "|" + e["offset"] + "|" + e["name"] + "|" + e["type"], file=outf)
